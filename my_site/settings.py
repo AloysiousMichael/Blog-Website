03230442b84d2a2e39be97ec87e451b802ed9503
AloysiousMichael <97792613+AloysiousMichael@users.noreply.github.com>
2025-03-07 19:32:32 +0530

    Update settings.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -30,9 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("IS_DEVELOPMENT",True) 
 
-ALLOWED_HOSTS = [
-    getenv("APP_HOST")
-]
+ALLOWED_HOSTS = ["blog-website-xbp0.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
